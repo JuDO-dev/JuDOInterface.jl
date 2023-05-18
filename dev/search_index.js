d60d28170c8d3e8b05195ec76ceaf425fc63f2b8
Documenter.jl <documenter@juliadocs.github.io>
2023-05-18 09:29:11 +0000

    build based on 2e5e309

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = JuDOInterface","category":"page"},{"location":"#JuDOInterface","page":"Home","title":"JuDOInterface","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for JuDOInterface.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [JuDOInterface]","category":"page"}]
+}
